f8cc9ca4e940f5c8e2671c9c7627ac2e4482e3e7
AviFischer <a.fischer1000@gmail.com>
2024-03-20 18:16:47 -0400

    Comment out safety check

--- src/state_machine/utils.py
+++ src/state_machine/utils.py
@@ -8,10 +8,10 @@ BIAS = -200
 def verify_shoulder_location(location:tuple[float, float]):
     if(not location):
         return False
-    if location[1] < ALLOWABLE_X_RANGE[0] or location[1] > ALLOWABLE_X_RANGE[1]:
-        return False
-    if location[0] < ALLOWABLE_Y_RANGE[0] or location[0] > ALLOWABLE_Y_RANGE[1]:
-        return False
+    #if location[1] < ALLOWABLE_X_RANGE[0] or location[1] > ALLOWABLE_X_RANGE[1]:
+        #return False
+    #if location[0] < ALLOWABLE_Y_RANGE[0] or location[0] > ALLOWABLE_Y_RANGE[1]:
+        #return False
     return True
 
 def webcam_to_gantry(location:tuple[float, float]):
