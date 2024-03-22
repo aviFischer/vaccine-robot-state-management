5d90dc615ea0001192bc17c9959af8807a421aa7
AviFischer <a.fischer1000@gmail.com>
2024-03-22 18:23:48 -0400

    Change scaling and bias

--- src/state_machine/utils.py
+++ src/state_machine/utils.py
@@ -2,8 +2,8 @@
 ALLOWABLE_X_RANGE = (300, 450)
 ALLOWABLE_Y_RANGE = (150, 400)
 
-SCALING_FACTOR = 1
-BIAS = -200
+SCALING_FACTOR = -1
+BIAS = 200
 
 def verify_shoulder_location(location:tuple[float, float]):
     if(not location):
